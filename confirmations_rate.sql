77fdce7cba7c9efaf26b3db58b42198c1406d16b
Rishav <rv1972002@gmail.com>
2026-03-10 12:54:55 +0530

    one more ques added

---
+++ confirmations_rate.sql
@@ -0,0 +1,3 @@
+select s.user_id, IFNULL(ROUND(sum(action='confirmed')/count(*), 2), 0.00) as confirmation_rate
+from Signups s left join Confirmations c 
+on s.user_id=c.user_id group by s.user_id;
