369d1f23897f24edd715625b7d125c581928566e
Rishav <rv1972002@gmail.com>
2026-03-11 19:29:23 +0530

    one more ques added

---
+++ avg_selling_price.sql
@@ -0,0 +1,5 @@
+select p.product_id, IFNULL(ROUND(sum(p.price*us.units)/sum(units), 2), 0)  as average_price 
+from Prices p 
+left join UnitsSold us 
+on p.product_id=us.product_id and us.purchase_date between p.start_date and p.end_date 
+group by p.product_id;
