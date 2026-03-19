911481eb0837d8ac36d5e31e2f6e965f4f31acc3
Rishav <rv1972002@gmail.com>
2026-03-19 13:13:04 +0530

    one more ques added

--- Advanced_Select_and_Joins/last_person_at_a_given_date.sql
+++ Advanced_Select_and_Joins/last_person_at_a_given_date.sql
@@ -9,3 +9,13 @@ from
 ) as temp 
 where cumWt<=1000 
 order by cumWt desc limit 1;
+
+
+-- approach : Using JOINS only
+select q1.person_name
+from Queue q1 inner join Queue q2
+on q1.turn>=q2.turn
+group by q1.turn
+having sum(q2.weight)<=1000
+order by sum(q2.weight) desc
+limit 1;
