f861f4136603643585f5117408ede338a1a60d58
Rishav <rv1972002@gmail.com>
2026-03-24 13:41:58 +0530

    one more ques added

---
+++ Subqueries/friend_request_II.sql
@@ -0,0 +1,11 @@
+select id, count(*) as num
+from (
+    select requester_id as id
+    from RequestAccepted
+    union all
+    select accepter_id as id
+    from RequestAccepted
+) as tmp
+group by id
+order by num desc
+limit 1;
