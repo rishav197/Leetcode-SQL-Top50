d075067fee3b23718a95b3a5fa82530c1a1a44f6
Rishav <rv1972002@gmail.com>
2026-04-16 14:51:16 +0530

    .md updated

--- Basic_Joins/confirmations_rate.sql
+++ Basic_Joins/confirmations_rate.sql
@@ -1,3 +1,11 @@
+-- Approach: Using IFNULL()
 select s.user_id, IFNULL(ROUND(sum(action='confirmed')/count(*), 2), 0.00) as confirmation_rate
 from Signups s left join Confirmations c 
 on s.user_id=c.user_id group by s.user_id;
+
+
+-- Approach: Without using IFNULL()
+select s.user_id, ROUND(sum(if(c.action='confirmed', 1, 0))/count(*), 2) as confirmation_rate 
+from Signups s left join Confirmations c 
+on s.user_id=c.user_id
+group by s.user_id;
