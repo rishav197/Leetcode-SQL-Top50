2f282881c780da48621930ffe45733d8fccded47
Rishav <rv1972002@gmail.com>
2026-04-30 19:53:56 +0530

    small update

--- Advanced_String_Functions_Regex_Clause/find_users_with_valid_emails.sql
+++ Advanced_String_Functions_Regex_Clause/find_users_with_valid_emails.sql
@@ -1,3 +1,4 @@
 select *
 from Users
-where mail regexp '^[A-Za-z][A-Za-z0-9_/./-]*@leetcode\\.com$' and mail like Binary '%@leetcode.com';
+where mail REGEXP '^[A-Za-z][A-Za-z0-9_/./-]*@leetcode\.com$'
+  and mail like binary '%@leetcode.com';
