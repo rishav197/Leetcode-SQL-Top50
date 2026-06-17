dff8cb7c723c016543cd624616b4fac002c3dad7
Rishav <rv1972002@gmail.com>
2026-06-17 19:07:49 +0530

    small update

--- Basic_Joins/managers_with_atleast_5_reports.sql
+++ Basic_Joins/managers_with_atleast_5_reports.sql
@@ -1,9 +1,9 @@
-select e1.name
-from Employee e1 inner join Employee e2 on e1.id=e2.managerID 
-group by e2.managerID having count(e2.managerID)>=5;
-
-
--- almost same but a bit diff
 select e1.name from Employee e1, Employee e2
 where e1.id=e2.managerId
 group by e1.id having count(*)>=5;
+
+
+-- approach : Almost same but a bit diff
+select e1.name
+from Employee e1 inner join Employee e2 on e1.id=e2.managerID 
+group by e2.managerID having count(e2.managerID)>=5;
