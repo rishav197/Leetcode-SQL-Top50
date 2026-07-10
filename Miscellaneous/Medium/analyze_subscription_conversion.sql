7ae86bdf8d27f1fb478d2291285fd8b7a4152d51
Rishav <rv1972002@gmail.com>
2026-07-10 16:30:59 +0530

    three more ques added

---
+++ Miscellaneous/Medium/analyze_subscription_conversion.sql
@@ -0,0 +1,12 @@
+select user_id, 
+ROUND(sum(if(activity_type='free_trial', activity_duration, 0))/sum(if(activity_type='free_trial', 1, 0)), 2) as trial_avg_duration,
+ROUND(sum(if(activity_type='paid', activity_duration, 0))/sum(if(activity_type='paid', 1, 0)), 2) as paid_avg_duration
+from UserActivity
+where user_id not in (
+    select user_id
+    from UserActivity
+    group by user_id
+    having sum(if(activity_type='free_trial' or activity_type='cancelled', 1,0))=count(*)
+)
+group by user_id
+order by user_id;
