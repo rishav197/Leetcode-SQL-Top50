f683c89d28065e7613c9b3cb85569c95180ba44a
Rishav <rv1972002@gmail.com>
2026-07-23 19:05:12 +0530

    .md updated

--- Miscellaneous/Medium/department_highest_salary.sql
+++ Miscellaneous/Medium/department_highest_salary.sql
@@ -1,3 +1,4 @@
+-- approach 1: using Co-related Subquery
 select  
     d.name as Department,
     e1.name as Employee,
@@ -9,3 +10,17 @@ where 1 = (
     from Employee e2
     where e2.Salary >= e1.Salary and e1.DepartmentId=e2.DepartmentId
 );
+
+
+-- approach 2: using Window Function 
+select Department, name as Employee, salary as Salary
+from (
+    select 
+        d.name as Department, e.name, e.salary, 
+        dense_rank() over(partition by d.name order by e.salary desc) as row_num
+    from Employee e inner join Department d 
+    on e.departmentId=d.id
+    order by e.salary desc
+) as temp
+where temp.row_num=1
+order by Department
