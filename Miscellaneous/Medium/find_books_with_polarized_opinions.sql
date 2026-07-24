0e29a72a16ec1b112ed1f3d402bace368a0eb32f
Rishav <rv1972002@gmail.com>
2026-07-24 13:15:52 +0530

    one more ques added

---
+++ Miscellaneous/Medium/find_books_with_polarized_opinions.sql
@@ -0,0 +1,17 @@
+with cte as (
+
+
+select book_id, SUM(IF(session_rating>=4,1,0)) as high_ratings,
+SUM(IF(session_rating<=2,1,0)) as low_ratings, (max(session_rating)-min(session_rating)) as rating_spread, 
+ROUND((SUM(IF(session_rating>=4,1,0)) + SUM(IF(session_rating<=2,1,0)))/count(*), 2) as polarization_score 
+from reading_sessions
+group by book_id
+having count(*)>=5
+and high_ratings>=1 and low_ratings>=1
+and polarization_score>=0.6
+)
+select c.book_id, b.title, b.author, b.genre, b.pages, c.rating_spread, c.polarization_score
+from cte c
+left join books b
+on c.book_id=b.book_id
+order by polarization_score desc, title desc;
